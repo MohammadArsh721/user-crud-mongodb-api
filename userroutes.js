88617626124206dfe40246871ebd71bfafffad2a
MohammadArsh721 <heyarshaly@gmail.com>
2025-07-11 17:03:36 +0530

    Add files via upload

---
+++ userroutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const {
+  createUser,
+  getAllUsers,
+  updateUser,
+  deleteUser
+} = require('../controllers/userController');
+
+const router = express.Router();
+
+router.post('/users', createUser);
+router.get('/users', getAllUsers);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
+
+module.exports = router;
